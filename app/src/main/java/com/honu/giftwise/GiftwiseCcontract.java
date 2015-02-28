f5f8503b8d48ee2500d787d00f2c6a340d112654
bdiegel <bdiegel@gmail.com>
2015-02-28 08:37:10 -0500

    create contract class for Uri support

---
+++ app/src/main/java/com/honu/giftwise/GiftwiseCcontract.java
@@ -0,0 +1,24 @@
+package com.honu.giftwise;
+
+import android.content.ContentUris;
+import android.net.Uri;
+
+/**
+ * Created by bdiegel on 2/27/15.
+ */
+public class GiftwiseCcontract {
+
+    // CONTENT_AUTHORITY is the base of all URI's
+    public static final String CONTENT_AUTHORITY = "com.honu.giftwise";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_CONTACTS = "contacts";
+
+    public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_CONTACTS).build();
+
+    // content://com.honu.giftwise/contacts/<contactId>
+    public static Uri buildContactUri(long contactId) {
+        return ContentUris.withAppendedId(CONTENT_URI, contactId);
+    }
+}
