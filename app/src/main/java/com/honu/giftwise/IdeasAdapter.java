335bb1ee5c194cc1c3064d27e8e8865cfed0cf05
bdiegel <bdiegel@gmail.com>
2015-03-14 16:05:00 -0400

    Show popup menu on imageview click

--- app/src/main/java/com/honu/giftwise/IdeasAdapter.java
+++ app/src/main/java/com/honu/giftwise/IdeasAdapter.java
@@ -4,7 +4,12 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.support.v7.widget.PopupMenu;
+import android.text.util.Linkify;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -16,6 +21,8 @@ import android.widget.TextView;
  */
 public class IdeasAdapter extends CursorAdapter {
 
+    private static final String LOG_TAG = IdeasAdapter.class.getSimpleName();
+
     public IdeasAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -25,7 +32,7 @@ public class IdeasAdapter extends CursorAdapter {
         // inflate view
         View view =  LayoutInflater.from(context).inflate(R.layout.list_item_gift, parent, false);
 
-        // wse ViewHolder to save inflated views:
+        // use ViewHolder to save inflated views:
         ViewHolder viewHolder = new ViewHolder(view);
         view.setTag(viewHolder);
 
@@ -43,17 +50,62 @@ public class IdeasAdapter extends CursorAdapter {
         viewHolder.priceView.setText(cursor.getString(cursor.getColumnIndex("price")));
     }
 
-    public static class ViewHolder{
+    public static class ViewHolder {
         public final ImageView iconView;
         public final TextView nameView;
         public final TextView urlView;
         public final TextView priceView;
+        public final ImageView menuView;
 
-        public ViewHolder(View view) {
+        public ViewHolder(final View view) {
             iconView = (ImageView) view.findViewById(R.id.list_item_gift_image);
             nameView = (TextView)view.findViewById(R.id.list_item_gift_name_textview);
             urlView = (TextView)view.findViewById(R.id.list_item_gift_url_textview);
             priceView = (TextView)view.findViewById(R.id.list_item_gift_price_textview);
+            menuView = (ImageView) view.findViewById(R.id.list_item_gift_overflow_icon);
+            Linkify.addLinks(urlView, Linkify.WEB_URLS);
+
+            menuView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i(LOG_TAG, "MENU PRESSED: " + view.getParent().toString());
+                    Log.i(LOG_TAG, "activity: " + view.getContext().toString());
+                    showPopup(v);
+                }
+            });
+
+            //((Activity)view.getContext()).registerForContextMenu(menuView);
+        }
+
+        public void showPopup(View v) {
+            Log.i(LOG_TAG, "Show popup");
+            PopupMenu popup = new PopupMenu(v.getContext(), v);
+
+            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.gift_edit:
+                            //archive(item);
+                            Log.i(LOG_TAG, "Edit pressed");
+                            return true;
+                        case R.id.gift_delete:
+                            Log.i(LOG_TAG, "Delete pressed");
+                            //delete(item);
+                            return true;
+                        case R.id.gift_open_url:
+                            Log.i(LOG_TAG, "Open url pressed");
+                            //delete(item);
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            });
+            MenuInflater inflater = popup.getMenuInflater();
+            inflater.inflate(R.menu.menu_gift_item, popup.getMenu());
+            popup.show();
         }
     }
+
 }
