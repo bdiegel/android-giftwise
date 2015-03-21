6102a5581f4da1cdae8089137082b71ea5e02cc7
bdiegel <bdiegel@gmail.com>
2015-03-21 14:16:24 -0400

    Provide methods to scale and resize bitmaps

--- app/src/main/java/com/honu/giftwise/data/BitmapUtils.java
+++ app/src/main/java/com/honu/giftwise/data/BitmapUtils.java
@@ -2,6 +2,7 @@ package com.honu.giftwise.data;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 
 import java.io.ByteArrayOutputStream;
 
@@ -20,5 +21,34 @@ public class BitmapUtils {
     // convert from byte array to bitmap
     public static Bitmap getImage(byte[] image) {
         return BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
+
+    public static Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
+    }
+
+    public static Bitmap createScaledBitmap(Bitmap bm) {
+        float aspectRatio = bm.getWidth() / (float) bm.getHeight();
+        //int width = 480;
+        int width = 480;
+        int height = Math.round(width / aspectRatio);
+
+//        // To use height as base intead of width change to:
+//        int height = 480;
+//        int width = Math.round(height * aspectRatio);
+
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bm, width, height, false);
+        return resizedBitmap;
     }
 }
